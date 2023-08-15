db2eae74fb1743fea4a191d4e313fffa58ce22c5
likeyeon <chy98csy00@naver.com>
2023-08-16 01:45:58 +0900

    project setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/Banner";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      Hello world
+    </div>
+  );
+}
+
+export default App;
